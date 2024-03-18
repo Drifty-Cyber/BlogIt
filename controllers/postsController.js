82eb8ecb73b51305b715f296c82b65d31b37eb1f
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-03-18 17:25:05 +0100

    chore: add controller functions

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,1 +1,11 @@
+const catchAsync = require('../utils/catchAsync');
 
+exports.getAllPosts = catchAsync(async (req, res, next) => {});
+
+exports.getPost = catchAsync(async (req, res, next) => {});
+
+exports.createPost = catchAsync(async (req, res, next) => {});
+
+exports.updatePost = catchAsync(async (req, res, next) => {});
+
+exports.deletePost = catchAsync(async (req, res, next) => {});
