a5c8dc6303a26f2c8067ce423faf1cf4d805ece1
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-03-27 18:57:21 +0100

    chore: add logout controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,3 +45,14 @@ exports.login = catchAsync(async (req, res, next) => {
 
   createSendToken(user, 200, req, res);
 });
+
+exports.logout = (req, res, next) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res
+    .status(200)
+    .json({ status: 'success', message: 'Logged out successfully' });
+};
